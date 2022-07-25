68db3d007d050ee9c204edfbe14b039f80a88e97
Raquel Stankey <brv1114@yahoo.com>
2022-07-24 20:45:40 -0500

    created tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(40) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    
+);
