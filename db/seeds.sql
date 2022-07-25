5a3bc70c4e0cd76d64e0748cb5f6885007083e6f
Raquel Stankey <brv1114@yahoo.com>
2022-07-24 21:06:27 -0500

    inserted information for tables in seed file

---
+++ db/seeds.sql
@@ -0,0 +1,60 @@
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CEO', 350000, 1),
+('COO', 305000, 1),
+('VP', 305000, 1),
+('CFO', 300000, 1),
+('OFFICER', 250000, 1),
+('DIRECTOR-ANY', 200000, 1),
+('CONTROLLER', 120000, 1),
+('MARKETING', 75000, 1),
+('SALES-REP', 65000, 1),
+('DRAFTER', 72000, 1),
+('EHS-PERSONNEL', 80000, 1),
+('ACCOUNTANT', 70000, 1),
+('MANUFACTURING-LABORER', 45000, 1),
+('ENGINEER', 130000, 1),
+('FIELD-OPERATIONS-LABORER', 62000, 1),
+('FOREPERSON', 75000, 1),
+('IT', 76000, 1);
+
+
+INSERT INTO department (dept_name)
+VALUES
+('EXECUTIVE'),
+('OPS-LEADERSHIP'),
+('SAFETY'),
+('SALES-&-MARKETING'),
+('FIELD'),
+('PLANT'),
+('IT'),
+('FINANCE'),
+('DRAFTING');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+(Leah, Villa, 1, 2),
+(Haley, Fernandes, 2, 5),
+(Paul, Nelson, 3, 4),
+(August, Payton, 4, NULL),
+(Ray, Raquel, 5, NULL),
+(Jay, Lee, 6, 10),
+(Amber, Marie, 7, 12),
+(Nicole, Maze, 8, NULL),
+(John, Doe, 9, 6),
+(DJ, Stalzer, 10, 8),
+(Jessica, Ringer, 11, 3),
+(Jayson, Jenkins, 12, 9),
+(Steven, Jacobs, 13, NULL),
+(Shakir, Hussan, 14, 16),
+(Roman, Rosario, 15, 11),
+(Zachary, Rosebud, 16, 8),
+(Ellen, Johnson, 17, 12),
+(Randy, Carlson, 18, 3),
+(Jeffrey, Jorgenson, 19, 4),
+(Jennifer, Nelson, 20, 7);
+
+
+
+
+
